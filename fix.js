49a2e473cd4da46224b52236ea74ed3ce73dee94
Alleexxi <66967403+Alleexxi@users.noreply.github.com>
2022-06-18 01:14:45 +0200

    Create fix.js

---
+++ fix.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         dx9 crash fix
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  version fix
+// @author       Alleexxii - Credits to Synctional
+// @match        https://www.roblox.com/games/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=roblox.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    if (!(Roblox.ProtocolHandlerClientInterface.playerChannel == ""))
+    {
+     Roblox.ProtocolHandlerClientInterface.playerChannel = "";
+    }
+})();
